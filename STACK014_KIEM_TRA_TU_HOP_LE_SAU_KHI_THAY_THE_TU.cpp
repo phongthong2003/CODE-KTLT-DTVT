7a1c5b74ab098055a780baff11f237c0226a06f0
Hoang Tran Phong <48386296+phongthong2003@users.noreply.github.com>
2023-11-12 10:18:07 +0700

    Add files via upload

---
+++ STACK014_KIEM_TRA_TU_HOP_LE_SAU_KHI_THAY_THE_TU.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool check(string s){
+    stack<char>st;
+    for(char c:s){
+        if(c=='a'){
+            st.push('b');
+        }else if(c=='b'){
+            st.push('c');
+        }else if(c=='c'){
+            if(st.empty()||st.top()!='c'){
+                return false;
+            }
+            st.pop();
+            if(st.top()!='b'||st.empty()){
+                return false;
+            }
+            st.pop();
+        }
+    }
+    return st.empty();
+}
+int main(){
+    int t;
+    cin >> t;
+    cin.ignore();
+    while(t--){
+        string s;
+        cin >> s;
+        if(check(s)==true){
+            cout <<"1"<<endl;
+        }else{
+            cout <<"0"<<endl;
+        }
+    }
+    return 0;
+}
