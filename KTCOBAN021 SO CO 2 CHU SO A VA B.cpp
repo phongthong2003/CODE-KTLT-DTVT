07803dee7fda7f8e7586efa9c87fa497025846ff
Hoang Tran Phong <48386296+phongthong2003@users.noreply.github.com>
2023-11-14 22:32:48 +0700

    KTCOBAN021 SO CO 2 CHU SO A VA B.cpp

--- KTCOBAN021 SO CO 2 CHU SO A VA B.cpp
+++ KTCOBAN021 SO CO 2 CHU SO A VA B.cpp
@@ -1,40 +1,40 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-    int t;
-    cin >> t;
-
-    while (t--) {
-        long long N;
-        int A, B;
-        cin >> N >> A >> B;
-
-        string result = "";
-        bool found = false;
-
-        for (int numA = 0; numA <= N; ++numA) {
-            int remaining = N - numA;
-            if (remaining % 2 == 0) {
-                int numB = remaining / 2;
-                string temp = string(numA, char('0' + A)) + string(numB, char('0' + B));
-                sort(temp.begin(), temp.end());
-                if (stoll(temp) >= N && (result.empty() || temp < result)) {
-                    result = temp;
-                    found = true;
-                }
-            }
-        }
-
-        if (found) {
-            cout << result << endl;
-        } else {
-            cout << -1 << endl;
-        }
+typedef unsigned long long ull;
+#define MAX 1000000000000000001
+ull Recursive (ull result, short x, int xcount, short y, int ycount, ull n, int sochuso){
+    if(xcount + ycount > sochuso + 2 || result > MAX){
+        return MAX;
     }
-
-    return 0;
+    if(xcount == ycount && result >= n){
+        return result;
+    }
+    return min(Recursive(result * 10 + x, x, xcount + 1, y, ycount, n, sochuso), Recursive(result * 10 + y, x, xcount, y, ycount + 1, n ,sochuso));
 }
 
+ull FindNum(ull n, short x, short y){
+    int sochuso = floor(log10(n)) + 1; 
+    ull result = 0; 
+    int xcount = 0; 
+    int ycount = 0; 
+    if (x == y){
+        while (result < n){
+            result = result * 10 + x;
+        }
+        return result;
+    }
+    return Recursive(result, x, xcount, y, ycount, n, sochuso);
+}
+
+int main (){
+    int test;
+    cin>>test;
+    while (test--){
+        ull n;
+        cin>>n;
+        short x, y;
+        cin>>x>>y;
+        cout<<FindNum(n ,x ,y)<<endl;
+    }
+    return 0;
+}
