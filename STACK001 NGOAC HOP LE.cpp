3bbd23acab263b6d8e006dc1b4413deebb4baa8a
Hoang Tran Phong <48386296+phongthong2003@users.noreply.github.com>
2023-11-17 22:16:08 +0700

    STACK001 NGOAC HOP LE.cpp

--- STACK001 NGOAC HOP LE.cpp
+++ STACK001 NGOAC HOP LE.cpp
@@ -1,30 +1,37 @@
-#include"bits/stdc++.h"
+#include <iostream>
+#include <stack>
 using namespace std;
-int main(){
-	int t;
-	cin >> t;
-	cin.ignore();
-	while(t--){
-		string s;
-		getline(cin,s);
-		stack<char>st;
-		bool correct = true;
-		for(char x:s){
-			if(x=='('||x=='{'||x=='['){
-				st.push(x);
-			}else{
-				if(st.empty()||x=='('&&st.top()!='('||x=='{'&&st.top()!='{'||x=='['&&st.top()!='['){
-					correct = false;
-					break;
-				}
-				st.pop();
-			}
-		}
-		if(correct && st.empty()){
-			cout <<"1"<<endl;
-		}else{
-			cout <<"0"<< endl;
-		}
-	}
-	return 0;
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string s;
+        getline(cin, s); 
+        stack<char> st;
+        bool isBalanced = true;
+
+        for (char x : s) {
+            if (x == '(' || x == '[' || x == '{') {
+                st.push(x);
+            } else {
+                if (st.empty() || 
+                    (x == ')' && st.top() != '(') ||
+                    (x == ']' && st.top() != '[') ||
+                    (x == '}' && st.top() != '{')) {
+                    isBalanced = false;
+                    break;
+                }
+                st.pop();
+            }
+        }
+
+        if (isBalanced && st.empty()) {
+            cout << "1" << endl;
+        } else {
+            cout << "0" << endl;
+        }
+    }
+    return 0;
 }
